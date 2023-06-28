2430c0a1fec611f179c9e2efcff81ecdf8a9efd9
Ulisses Duo <duoulisses@gmail.com>
2023-06-27 21:09:24 -0300

    login register working

---
+++ baltaIOCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using baltaIOCrud.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace baltaIOCrud.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+    }
+}
---
+++ baltaIOCrud/Migrations/20230624002613_database setup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace baltaIOCrud.Migrations
+{
+    /// <inheritdoc />
+    public partial class databasesetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Testes");
+
+            migrationBuilder.CreateTable(
+                name: "SalesLead",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesLead", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesLead");
+
+            migrationBuilder.CreateTable(
+                name: "Testes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testes", x => x.Id);
+                });
+        }
+    }
+}
