8eaf3eacc0cec252471f7e2c895aa8abaaf861ba
Ulisses Duo <duoulisses@gmail.com>
2023-06-29 22:57:03 -0300

    complete crud products

--- baltaIOCrud/Controllers/ProductsController.cs
+++ baltaIOCrud/Controllers/ProductsController.cs
@@ -22,8 +22,6 @@ namespace baltaIOCrud.Controllers
             return View(x);
         }
 
-
-
         public IActionResult Add()
         {
             var typeProducts = TypeProducts.GetAll();
@@ -55,5 +53,86 @@ namespace baltaIOCrud.Controllers
             await _context.SaveChangesAsync();//save the changes
             return RedirectToAction("Index");//return to index page after insert
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid Id) 
+        {
+            var products = await _context.Products.FirstOrDefaultAsync(x => x.Id == Id);//find the first record that matches with the id parameter
+
+            if (products != null)//check if the data retrieved has been found 
+            {
+                var typeProducts = TypeProducts.GetAll();//get all the types of products generated dinamically
+                var viewModel = new UpdateProductViewModel()//initialize the productViewModel with its properties values as retrieved from 'products' object
+                {
+                    Id = Guid.NewGuid(),//set values to properties to update
+                    Name = products.Name,
+                    Price = products.Price,
+                    Description = products.Description,
+                    Category = products.Category,
+                    StockQuantity = products.StockQuantity,
+                    TypeProductList = typeProducts.Select(typeProducts => new SelectListItem 
+                    {
+                        Text = typeProducts.Name,
+                        Value = typeProducts.Name,
+                        Selected = typeProducts.Name == products.Category
+                    }).ToList(),//transform the object into a list
+                };
+                return View("Edit", viewModel);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateProductViewModel model) 
+        {
+            var selectedCategory = model.SelectedCategory;
+            var product = await _context.Products.FindAsync(model.Id);
+            if (product != null)
+            {
+                product.Name = model.Name;
+                product.Price = model.Price;
+                product.Description = model.Description;
+                product.Category = selectedCategory;
+                product.StockQuantity = model.StockQuantity;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid? Id) 
+        {
+            if (Id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FirstOrDefaultAsync(x => x.Id == Id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateProductViewModel model) 
+        {
+            var products = await _context.Products.FindAsync(model.Id);
+
+            if (products != null)
+            {
+
+                _context.Products.Remove(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
